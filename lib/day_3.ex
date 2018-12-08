4b0d0e43d40f7d3a4c6ee00e4eeada4496895c3a
Anderson Cook <acook@aspirehealthcare.com>
2018-12-08 17:37:31 -0600

    day3 pt2 v1

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -26,17 +26,51 @@ defmodule AOC.Day3 do
     |> Enum.reduce(0, fn {_, n}, acc -> acc + n end)
   end
 
+  @doc """
+  iex> AOC.Day3.part_2([
+  ...> %{id: 1, x: 1, y: 3, width: 4, height: 4},
+  ...> %{id: 2, x: 3, y: 1, width: 4, height: 4},
+  ...> %{id: 3, x: 5, y: 5, width: 2, height: 2},
+  ...> ])
+  3
+  """
+  def part_2(input \\ input()) do
+    input
+    |> Task.async_stream(fn c ->
+      for x <- c.x..(c.x + c.width - 1),
+          y <- c.y..(c.y + c.height - 1),
+          into: %{},
+          do: {{x, y}, [c.id]}
+    end)
+    |> Enum.reduce(%{}, fn {:ok, c}, acc ->
+      Map.merge(acc, c, fn _, c1s, c2s -> c1s ++ c2s end)
+    end)
+    |> Map.values()
+    |> Enum.split_with(fn v -> Kernel.length(v) == 1 end)
+    |> (fn {singles, rest} ->
+          rest =
+            rest
+            |> List.flatten()
+            |> Enum.uniq()
+
+          singles
+          |> List.flatten()
+          |> Enum.uniq()
+          |> Enum.find(fn s -> s not in rest end)
+        end).()
+  end
+
   defp input do
     "txts/day_3.txt"
     |> File.read!()
     |> String.split("\n")
     |> Task.async_stream(fn string ->
-      [_id, x, y, width, height] =
+      [id, x, y, width, height] =
         string
         |> String.split(~r{\D}, trim: true)
         |> Enum.map(&String.to_integer/1)
 
-      %{x: x, y: y, width: width, height: height}
+      %{id: id, x: x, y: y, width: width, height: height}
     end)
     |> Enum.map(&Kernel.elem(&1, 1))
   end
